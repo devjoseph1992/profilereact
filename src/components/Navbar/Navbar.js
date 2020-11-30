2779f3f817c2f7589b27dd1ecc71a8d2dcc302ba
john joseph florina <devjosephflorina@gmail.com>
2020-11-30 23:04:50 +0800

    first commit finish navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+
+import "./Navbar.css";
+
+import { Link } from "react-router-dom";
+import { BiMenu, BiArrowFromRight } from "react-icons/bi";
+
+import { Button } from "../Button/Button";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            JHNJSPH
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? (
+              <BiArrowFromRight color="white" />
+            ) : (
+              <BiMenu color="white" />
+            )}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/skills"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Skills
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/portfolio"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">Contact</Button>}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
